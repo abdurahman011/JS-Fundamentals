945d4712bd7f231ac55ebb1af9018d7fee374cb1
abdurahman <abdure1080@gmail.com>
2025-09-14 23:54:32 +0300

    üpdated factorial

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,10 +1,18 @@
 const factorial = (n) => {
-  return isNaN(n) || n === 0 ? 1 : n * factorial(n - 1);
+  if (isNaN(n) || n < 0) {
+    return 1;
+  }
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 };
 
 const num = parseInt(process.argv[2]);
 
-if (isNaN(num)) {
+if (process.argv.length < 3) {
+  console.log("No argument");
+} else if (isNaN(num)) {
   console.log("No argument");
 } else {
   console.log(factorial(num));
